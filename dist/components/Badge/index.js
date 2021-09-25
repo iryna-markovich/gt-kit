55bf4c5d6ed1232104256a0f643b3a1c9a0538c2
iryna-markovich-vocovo <iryna.markovich@vocovo.com>
2021-09-26 02:46:51 +0300

    upd

---
+++ dist/components/Badge/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _config = _interopRequireDefault(require("../../../config"));
+
+require("./index.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const lib = _config.default.lib;
+
+const Badge = props => {
+  const {
+    children,
+    className,
+    id,
+    size,
+    type
+  } = props;
+  const badgeType = type ? type : 'default';
+  const badgeSize = size ? size : 'm';
+  const classNames = className ? className : '';
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: "".concat(lib, "-badge ").concat(lib, "-badge--").concat(badgeType, " ").concat(lib, "-badge--").concat(badgeSize, " ").concat(classNames),
+    "data-id": id
+  }, children);
+};
+
+var _default = Badge;
+exports.default = _default;
---
+++ src/lib/components/Badge/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import config from '../../../config'
+import './index.css'
+
+const lib = config.lib
+
+const Badge = (props) => {
+  const { children, className, id, size, type } = props
+  const badgeType = type ? type : 'default'
+  const badgeSize = size ? size : 'm'
+  const classNames = className ? className : ''
+
+  return (
+    <div
+      className={`${lib}-badge ${lib}-badge--${badgeType} ${lib}-badge--${badgeSize} ${classNames}`}
+      data-id={id}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Badge
